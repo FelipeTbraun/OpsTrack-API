86fc70dbf014fc7baf2c0fe0bd2af6ad6333d26c
FelipeTbraun <tbraunfelipe@gmail.com>
2026-08-24 23:40:49 -0300

    Refactor: movi a lista de tickets para o topo do arquivo

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+@app.route('/tickets')
+def tickets():
+    mocked_tickets = [
+        {'id': 1, 'titulo': 'Impressora sem conexão', 'status': 'aberto'},
+        {'id': 2, 'titulo': 'Erro ao acessar internet', 'status': 'em andamento'},
+        {'id': 3, 'titulo': 'Solicitação de novo monitor', 'status': 'fechado'},
+    ]
+    
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
@@ -11,13 +18,7 @@ def hello_world():  # put application's code here
 def status():
     return {'status':'Ok'}
 
-@app.route('/tickets')
-def tickets():
-    mocked_tickets = [
-        {'id': 1, 'titulo': 'Impressora sem conexão', 'status': 'aberto'},
-        {'id': 2, 'titulo': 'Erro ao acessar VPN', 'status': 'em andamento'},
-        {'id': 3, 'titulo': 'Solicitação de novo monitor', 'status': 'fechado'},
-    ]
+
     return {'tickets': mocked_tickets}
 
 if __name__ == '__main__':
