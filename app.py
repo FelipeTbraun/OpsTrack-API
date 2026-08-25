1258abf283ca2fb00f9b2b6546a67cc4061a52e6
FelipeTbraun <tbraunfelipe@gmail.com>
2026-08-24 23:38:41 -0300

    feat:Adicionei rota /tickets com lista de chamados

--- app.py
+++ app.py
@@ -9,7 +9,16 @@ def hello_world():  # put application's code here
 
 @app.route('/')
 def status():
-    return 'Okl'
+    return {'status':'Ok'}
+
+@app.route('/tickets')
+def tickets():
+    mocked_tickets = [
+        {'id': 1, 'titulo': 'Impressora sem conexão', 'status': 'aberto'},
+        {'id': 2, 'titulo': 'Erro ao acessar VPN', 'status': 'em andamento'},
+        {'id': 3, 'titulo': 'Solicitação de novo monitor', 'status': 'fechado'},
+    ]
+    return {'tickets': mocked_tickets}
 
 if __name__ == '__main__':
     app.run()
